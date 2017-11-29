5e1764f778816b6540dc360e0231f3b2bd860c75
mehaotian <490272692@qq.com>
2017-11-29 19:08:30 +0800

    微信小程序索引列表初始代码提交

---
+++ pages/index/index.js
@@ -0,0 +1,49 @@
+const CityIndexList = require('../../wx-list-index/List-index.js')
+
+Page({
+  data: {
+
+  },
+  onReady: function () {
+
+    var city = new CityIndexList(this);
+
+
+    // 数据格式 （严格）
+    let arr = [
+      {
+        title: "⭐",
+        item: [
+          {
+            name: "123",
+            key: "A"
+          }
+        ]
+
+      },
+      {
+        title: "A",
+        item: [
+          {
+            name: "234",
+            key: "B"
+          }
+        ]
+
+      }
+    ]
+    // 重置数据
+    city.setting(function (set) {
+      // set.data(arr); 
+    })
+    // 点击事件
+    city.tap(function (e) {
+      console.log(e)
+    })
+
+
+
+  }
+
+
+})
---
+++ wx-list-index/wx-list-index.js
@@ -0,0 +1,2 @@
+// wx-list-index/wx-list-index.js
+Page({})
